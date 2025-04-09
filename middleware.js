a88214de0c2239bf343f8d554d5dade2312115a1
Dhanushkumar13 <dhanushpappu99@gmail.com>
2025-04-09 15:57:26 +0530

    added authMiddleware

---
+++ middleware.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = require('./config');
+
+const authMiddleware = (req,res,next) =>{
+    //check for authorization token
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        return res.status(403).json({
+            message: "Invalid token"
+        })
+    }
+
+    //Getting the token value out of bearer token
+    const token = authHeader.split(' ')[1];
+
+    try {
+        //verify the jwt using JWT_SECRET
+        const decoded = jwt.verify(token,JWT_SECRET);
+
+        if(decoded.userId){
+            req.userId = decoded.userId;
+            next();
+        }else{
+            return res.status(403).json({
+                message: "Invalid token"
+            })
+        }
+    } catch (error) {
+        return res.status(403).json({
+            message: "Invalid token"
+        })
+    }
+}
+
+module.exports = {
+    authMiddleware
+};
+
